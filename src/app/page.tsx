0c75c8056f782fb0ff8474c74992f3b5657eeeab
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-11-01 00:38:23 -0300

    Página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+// Styles
 import styles from './styles/page.module.scss'
-import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.title}>Primeiro segundo projeto com <span>NextJs</span></h1>
-      <Link href='/contato'>Contato</Link>
+    <main className={styles.contentContainer}>
+      <img src="images/board-user.svg" alt="Ferramenta Board" className={styles.banner} />
+      <section className={styles.callToAction}>
+        <h1>Uma ferramenta para o seu dia a dia. Escreva, planeje e organize...</h1>
+        <p>
+          <span>100% Gratuita </span>
+          e online.
+        </p>
+      </section>
+
+      <div className={styles.donaters}>
+        <h3>Apoiadores:</h3>
+        <img src="images/Perfil.jpg" alt="Usuário 1" />
+        <img src="images/Perfil.jpg" alt="Usuário 2" />
+        <img src="images/Perfil.jpg" alt="Usuário 3" />
+        <img src="images/Perfil.jpg" alt="Usuário 4" />
+        <img src="images/Perfil.jpg" alt="Usuário 5" />
+        <img src="images/Perfil.jpg" alt="Usuário 6" />
+        <img src="images/Perfil.jpg" alt="Usuário 7" />
+      </div>
     </main>
   )
 }
